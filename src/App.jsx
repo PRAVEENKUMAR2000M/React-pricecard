3ccc4a25d5d295bfe8c76df524843db6e8bf5a4d
PRAVEENKUMAR M <131171871+PRAVEENKUMAR2000M@users.noreply.github.com>
2023-10-24 19:50:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,57 @@ function App(props) {
         <input />
         <button onClick={addNote}>save notes</button>
       </form>
+
+
+
+
+      import { Component } from "react";
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    }
+  }
+  // incremental = () => {
+  //   this.setState({
+  //     count: this.state.count +1,
+  //   })
+  // }
+
+  Plushandle = () => {
+    this.setState((prevState) => ({
+      count: prevState.count +1,
+    })) 
+}
+
+  
+    minushandle = () => {
+    this.setState((prevState) => ({
+      count: prevState.count -1,
+    })) 
+  }
+  
+      Zerohandle = () => {
+    this.setState((prevState) => ({
+      count: 0,
+    })) 
+}
+
+  render() {
+    return (
+      <div>
+        <p>count: {this.state.count} </p>
+        <button onClick={this.Plushandle}>Plushandle</button>
+        <button onClick={this.minushandle}>minushandle</button>
+        <button onClick={this.Zerohandle}>Zerohandle</button>
+      </div>
+    )
+  }
+}
+export default App;
     </div>
   );
 }

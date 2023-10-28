ba294b4e3504a8eadb6b42e1d8f17fa2eddf5576
praveenkumar <praveenmech483@gmail.com>
2023-10-28 18:25:44 +0530

    task completed

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,7 @@ import App from './App.jsx';
 
 
 
-const books = [
-  {
-    id: 1,
-    content: "praveenkumar BE",
-    important: true
-  },
-  {
-    id: 2,
-    content: "IT support engineer",
-    important: true
-  },
-  {
-    id: 3,
-    content: "Eduction background was BE mechanical engineer",
-    imoprtant: false
-  },
-  {
-    id: 4,
-    content: "now full stack developer",
-    important: false
-  },
-];
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
